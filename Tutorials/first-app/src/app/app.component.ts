35aa73e79c423419b88a9b46e90bfa9627faa71c
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-06 13:54:23 +0700

    Lesson 10 : Step 2 - Add routing to the application

--- Tutorials/first-app/src/app/app.component.ts
+++ Tutorials/first-app/src/app/app.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // imports: [RouterOutlet],
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   // templateUrl: './app.component.html',
   template: `
     <main>
-      <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          aria-hidden="true"
-        />
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
