44b9a569be5d857658e0ae9386e11d338a236f12
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-07 09:22:35 +0700

    Tour of Heroes : The hero editor : Create the heroes component

--- Tutorials/angular-tour-of-heroes/src/app/app.component.ts
+++ Tutorials/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   // styleUrl: './app.component.css',
   styleUrls: ['./app.component.css'],
+  imports: [HeroesComponent],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
