50d9504d55de73b65f25f9074967107ee5f7c126
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-08 09:48:54 +0700

    Tour of Heroes : Create a feature component

---
+++ Tutorials/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,16 @@
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [FormsModule, NgIf, UpperCasePipe],
+  templateUrl: './hero-detail.component.html',
+  // styleUrl: './hero-detail.component.css',
+  styleUrls: ['./hero-detail.component.css'],
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
--- Tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HEROES } from '../mock-heroes';
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
+  imports: [HeroDetailComponent, NgFor],
   templateUrl: './heroes.component.html',
   // styleUrl: './heroes.component.css'
   styleUrls: ['./heroes.component.css'],
@@ -19,7 +19,7 @@ export class HeroesComponent {
   // };
   heroes = HEROES;
   selectedHero?: Hero;
-  
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
