5e75ac65e2cb0d6768740c467981fad2fdb7864f
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-08 09:27:47 +0700

    Tour of Heroes : Display a selection list : Style the heroes

--- Tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Tutorials/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HEROES } from '../mock-heroes';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [NgFor],
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
   templateUrl: './heroes.component.html',
   // styleUrl: './heroes.component.css'
   styleUrls: ['./heroes.component.css'],
@@ -16,4 +18,9 @@ export class HeroesComponent {
   //   name: 'Windstorm',
   // };
   heroes = HEROES;
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
