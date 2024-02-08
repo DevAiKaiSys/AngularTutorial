46474c9e13b822206e23372aaa961e2884307732
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-08 23:21:34 +0700

    Tour of Heroes : Get data from a server : Update heroes

--- Tutorials/angular-tour-of-heroes/src/app/hero.service.ts
+++ Tutorials/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,13 +3,17 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, catchError, of, tap } from 'rxjs';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
   private heroesUrl = 'api/heroes'; // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
   constructor(
     private http: HttpClient,
@@ -42,6 +46,14 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap((_) => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
