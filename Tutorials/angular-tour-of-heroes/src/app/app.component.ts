f85c643ea52329332761d5a6182e2b35cef9d174
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-08 11:44:38 +0700

    Tour of Heroes : Add navigation with routing : Add RouterOutlet

--- Tutorials/angular-tour-of-heroes/src/app/app.component.ts
+++ Tutorials/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import { MessagesComponent } from './messages/messages.component';
   templateUrl: './app.component.html',
   // styleUrl: './app.component.css',
   styleUrls: ['./app.component.css'],
-  imports: [HeroesComponent, MessagesComponent],
+  imports: [RouterOutlet, MessagesComponent],
 })
 export class AppComponent {
   title = 'Tour of Heroes';
