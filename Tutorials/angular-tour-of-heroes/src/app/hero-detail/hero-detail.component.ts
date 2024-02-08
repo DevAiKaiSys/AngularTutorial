cbc85932238041b98e8242b265163cd40b5c5d0a
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-08 12:45:06 +0700

    Tour of Heroes : Add navigation with routing : Routable HeroDetailComponent

--- Tutorials/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Tutorials/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { NgIf, UpperCasePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -11,6 +14,25 @@ import { Hero } from '../hero';
   // styleUrl: './hero-detail.component.css',
   styleUrls: ['./hero-detail.component.css'],
 })
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
