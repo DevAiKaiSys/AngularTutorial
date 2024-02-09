36aebc968b498fef0b7ebdfcce548cb20d528b3c
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-02-09 12:45:20 +0700

    Building a template-driven form : Build the form

--- Tutorials/forms/src/app/hero-form/hero-form.component.ts
+++ Tutorials/forms/src/app/hero-form/hero-form.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-hero-form',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './hero-form.component.html',
   // styleUrl: './hero-form.component.css',
   styleUrls: ['./hero-form.component.css'],
 })
-export class HeroFormComponent {}
+export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  skyDog(): Hero {
+    const myHero = new Hero(
+      42,
+      'SkyDog',
+      'Fetch any object at any distance',
+      'Leslie Rollover'
+    );
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
+    return myHero;
+  }
+}
