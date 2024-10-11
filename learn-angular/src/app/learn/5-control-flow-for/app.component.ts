0e45aa8d9382a399ce718164c8a2a859fb1efaff
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-10-11 19:12:59 +0700

    Control flow - @for

---
+++ learn-angular/src/app/learn/5-control-flow-for/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // starting
+  /* template: ``, */
+  // completed
+  template: `
+    @for(user of users; track user.id) {
+    <p>{{ user.name }}</p>
+    }
+  `,
+  standalone: true,
+})
+export class AppComponent {
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
+}
