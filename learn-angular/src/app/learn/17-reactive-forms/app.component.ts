96c3d6b8b15de2ded1a4911935c9aafa5a4d22d1
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-10-11 21:02:08 +0700

    Reactive forms

---
+++ learn-angular/src/app/learn/17-reactive-forms/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  // starting
+  /* template: `
+    <form>
+      <label>
+        Name
+        <input type="text" formControlName="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" formControlName="email" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  `,
+  standalone: true,
+  imports: [], */
+  // completed
+  template: `
+    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
+      <input type="text" formControlName="name" />
+      <input type="email" formControlName="email" />
+      <button type="submit">Submit</button>
+    </form>
+
+    <h2>Profile Form</h2>
+    <p>Name: {{ profileForm.value.name }}</p>
+    <p>Email: {{ profileForm.value.email }}</p>
+  `,
+  standalone: true,
+  imports: [ReactiveFormsModule],
+})
+export class AppComponent {
+  // starting
+  // completed
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  handleSubmit() {
+    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
+}
